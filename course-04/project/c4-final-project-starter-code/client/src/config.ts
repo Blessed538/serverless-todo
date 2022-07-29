95ec08db97b1011f35d889638b26111b1b3f5b8c
Blessed538 <blesseddominic98@gmail.com>
2022-07-29 14:34:51 +0100

    split business logic

--- course-04/project/c4-final-project-starter-code/client/src/config.ts
+++ course-04/project/c4-final-project-starter-code/client/src/config.ts
@@ -2,6 +2,7 @@
 const apiId = 'pttlzl3cad'
 export const apiEndpoint = `https://${apiId}.execute-api.us-east-2.amazonaws.com/dev`
 
+
 export const authConfig = {
   // TODO: Create an Auth0 application and copy values from it into this map. For example:
   // domain: 'dev-nd9990-p4.us.auth0.com',
@@ -9,3 +10,9 @@ export const authConfig = {
   clientId: 'U1sFb8R6UmJVVrpaKU0hi1n7avvWR0hx',          // Auth0 client id
   callbackUrl: 'http://localhost:3000/callback'
 }
+
+// GET - https://pttlzl3cad.execute-api.us-east-2.amazonaws.com/dev/todos
+// POST - https://pttlzl3cad.execute-api.us-east-2.amazonaws.com/dev/todos
+// PATCH - https://pttlzl3cad.execute-api.us-east-2.amazonaws.com/dev/todos/{todoId}
+// DELETE - https://pttlzl3cad.execute-api.us-east-2.amazonaws.com/dev/todos/{todoId}
+// POST - https://pttlzl3cad.execute-api.us-east-2.amazonaws.com/dev/todos/{todoId}/attachment
