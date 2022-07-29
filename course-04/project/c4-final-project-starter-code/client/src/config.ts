240394ee858971a5c866e84fc8490f9c9d9f4285
Blessed538 <blesseddominic98@gmail.com>
2022-07-29 15:00:08 +0100

    modified to suite changes

--- course-04/project/c4-final-project-starter-code/client/src/config.ts
+++ course-04/project/c4-final-project-starter-code/client/src/config.ts
@@ -1,7 +1,6 @@
 // TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
-const apiId = 'pttlzl3cad'
+const apiId = 'igcp86t76g'
 export const apiEndpoint = `https://${apiId}.execute-api.us-east-2.amazonaws.com/dev`
-
 
 export const authConfig = {
   // TODO: Create an Auth0 application and copy values from it into this map. For example:
@@ -10,9 +9,3 @@ export const authConfig = {
   clientId: 'U1sFb8R6UmJVVrpaKU0hi1n7avvWR0hx',          // Auth0 client id
   callbackUrl: 'http://localhost:3000/callback'
 }
-
-// GET - https://pttlzl3cad.execute-api.us-east-2.amazonaws.com/dev/todos
-// POST - https://pttlzl3cad.execute-api.us-east-2.amazonaws.com/dev/todos
-// PATCH - https://pttlzl3cad.execute-api.us-east-2.amazonaws.com/dev/todos/{todoId}
-// DELETE - https://pttlzl3cad.execute-api.us-east-2.amazonaws.com/dev/todos/{todoId}
-// POST - https://pttlzl3cad.execute-api.us-east-2.amazonaws.com/dev/todos/{todoId}/attachment
